bf9a363f4bc9a711926aab7b8f1c504f345fb1f4
Enrique <enriqueivanr@gmail.com>
2025-06-18 13:26:50 -0600

    Cargar y visualizar imagen

--- Main.py
+++ Main.py
@@ -2,3 +2,16 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 from skimage import exposure
+
+# Cargar la imagen
+image = cv2.imread('foto1.jpg')
+
+# Convertir de BGR a RGB para visualización
+image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+# Mostrar la imagen original
+plt.figure(figsize=(10, 10))
+plt.imshow(image_rgb)
+plt.title('Imagen Original')
+plt.axis('off')
+plt.show()
